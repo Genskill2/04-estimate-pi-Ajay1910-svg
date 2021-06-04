37c4fa3891b0d33bef83944d3c1cafea4e63fd6e
Ajay1910-svg <84716743+Ajay1910-svg@users.noreply.github.com>
2021-06-04 08:03:48 +0530

    Add files via upload

--- monte_carlo.c
+++ monte_carlo.c
@@ -4,13 +4,12 @@
 #include <math.h>
 
 float mc_pi(int);
+
 float frandom() {
   long int q = random();
   float ret = (float)q/(float)RAND_MAX;
   return ret;
 }
-
-
 
 int main(void) {
   float pi0;
@@ -40,25 +39,5 @@ int main(void) {
   }
 }
 
-float mc_pi(int n)
-{
-  int circle=0;
-  int square=0;
-  float x,y;
-  for(int i=1;i<=n;i++)
-  {
-    x=frandom();
-    y=frandom();
-    if(((x*x)+(y*y))<=1)
-    {circle++;}
-    else{square++;}
-  }square=square+circle;
-    float pi=4*(float)circle/square;
-  return pi;
-}
-  
-      
 
-    
-    
 
--- wallis.c
+++ wallis.c
@@ -21,17 +21,6 @@ int main(void) {
       printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
       abort();
     }
-  } 
-}
-
-
-float wallis_pi(int n)
-{
-  float p=1;
-  for(float n=1;n<=1;n++)
-  {
-    p=p*((4*n*n)/((4*n*n)-1));
   }
-  p=2*p;
-  return p;
 }
+
